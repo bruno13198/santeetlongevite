885eba46588fba5dcd9ebf7f10c22428ecc981fe
bruno13198 <lopezbruno13@yahoo.fr>
2026-08-04 11:18:03 +0200

    Add Google verification to metadata
    Added Google verification meta tag for SEO.

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 export const metadata = {
   title: 'Super Aliments Santé',
   description: 'Études scientifiques sur les aliments et la santé',
+  verification: {
+    google: 'bcKpWEIEXZZai9xkX-fSanCo3fa1MoPSvQTDxQ7knFs',
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
