4024eafe45e070daa9be51df42dee0244389fa50
bruno13198 <lopezbruno13@yahoo.fr>
2026-08-19 09:04:06 +0200

    Integrate Google Analytics in RootLayout
    Added Google Analytics script to RootLayout.

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 export const metadata = {
   title: 'Super Aliments Santé',
   description: 'Études scientifiques sur les aliments et la santé',
@@ -5,10 +7,25 @@ export const metadata = {
     google: 'bcKpWEIEXZZai9xkX-fSanCo3fa1MoPSvQTDxQ7knFs',
   },
 };
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F6HGY41SC6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F6HGY41SC6');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
