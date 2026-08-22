df09da79b1cde15418b8234664e3f70617f2b763
bruno13198 <lopezbruno13@yahoo.fr>
2026-08-22 17:15:12 +0200

    Add Nav component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
+        <Nav />
         {children}
         <Footer />
         <CookieConsent />
