004c98fde89491a91fec658f72117928c6b24d69
bruno13198 <lopezbruno13@yahoo.fr>
2026-08-22 17:12:49 +0200

    Add navigation component with links

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,1 +1,20 @@
+import Link from 'next/link';
+import styles from './Nav.module.css';
 
+export default function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.wrap}>
+        <ul className={styles.left}>
+          <li><Link href="/" className={styles.link}>Accueil</Link></li>
+          <li><Link href="/articles" className={styles.link}>Articles</Link></li>
+          <li><Link href="/veille-scientifique" className={styles.link}>Veille scientifique</Link></li>
+        </ul>
+        <ul className={styles.right}>
+          <li><Link href="/a-propos" className={styles.link}>À propos</Link></li>
+          <li><Link href="/articles/Pourquoi-ce-site" className={styles.link}>Pourquoi ce site</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
