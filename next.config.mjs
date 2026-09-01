3df8586046477329b5ad1940f7499d609de42d16
bruno13198 <lopezbruno13@yahoo.fr>
2026-09-01 21:13:06 +0200

    Add redirect for specific host to new destination

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'santeetlongevite.vercel.app',
+          },
+        ],
+        destination: 'https://sciencetruths.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
+export default nextConfig
