0aaaf54e75a4b6dcec17817409532155f79ac844
bruno13198 <lopezbruno13@yahoo.fr>
2026-09-13 15:39:31 +0200

    Fix syntax error in GROUPES declaration

--- scripts/fusionner-aliments.js
+++ scripts/fusionner-aliments.js
@@ -15,7 +15,6 @@ const supabase = createClient(
 const MODE_SIMULATION = (process.env.MODE_SIMULATION || 'true') !== 'false';
 
 const GROUPES = [
-  const GROUPES = [
   { garder: 'fromage-de-chevre-a-pate-pressee-non-cuite-type-tomme-ou-tome-ou-gouda-de-chevre', absorber: ['selles-sur-cher-fromage-de-chevre','chabichou-fromage-de-chevre','fromage-de-chevre-a-pate-molle-et-croute-fleurie-type-camembert-cabecou','pouligny-saint-pierre-fromage-de-chevre','crottin-de-chavignol-fromage-de-chevre','sainte-maure-de-touraine-fromage-de-chevre','picodon-fromage-de-chevre','valencay-fromage-de-chevre','sainte-maure-fromage-de-chevre','fromage-de-chevre-a-pate-molle-non-pressee-non-cuite-et-croute-naturelle','rocamadour-fromage-de-chevre','chevrot-fromage-de-chevre','crottin-de-chevre-au-lait-cru','fromage-de-chevre-demi-sec','fromage-de-chevre-sec','pelardon-fromage-de-chevre'] },
 ];
 
